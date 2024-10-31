1469af874220c9520e0b87d232c106eec3589342
tanabee <tanabee1986@gmail.com>
2024-10-31 21:54:12 +0900

    Minimize

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,20 @@
-import * as z from 'zod';
-
-// Import the Genkit core libraries and plugins.
-import { generate } from '@genkit-ai/ai';
-import { configureGenkit } from '@genkit-ai/core';
-import { defineFlow, startFlowsServer } from '@genkit-ai/flow';
-import { ollama } from 'genkitx-ollama';
+import * as z from 'zod'
+import { generate } from '@genkit-ai/ai'
+import { configureGenkit } from '@genkit-ai/core'
+import { defineFlow, startFlowsServer } from '@genkit-ai/flow'
+import { ollama } from 'genkitx-ollama'
 
 configureGenkit({
   plugins: [
     ollama({
-      // Ollama provides an interface to many open generative models. Here,
-      // we specify Google's Gemma model. The models you specify must already be
-      // downloaded and available to the Ollama server.
       models: [{ name: 'gemma' }],
-      // The address of your Ollama API server. This is often a different host
-      // from your app backend (which runs Genkit), in order to run Ollama on
-      // a GPU-accelerated machine.
       serverAddress: 'http://127.0.0.1:11434',
     }),
   ],
-  // Log debug output to tbe console.
   logLevel: 'debug',
-  // Perform OpenTelemetry instrumentation and enable trace collection.
   enableTracingAndMetrics: true,
-});
+})
 
-// Define a simple flow that prompts an LLM to generate menu suggestions.
 export const menuSuggestionFlow = defineFlow(
   {
     name: 'menuSuggestionFlow',
@@ -33,24 +22,16 @@ export const menuSuggestionFlow = defineFlow(
     outputSchema: z.string(),
   },
   async (subject) => {
-		// Construct a request and send it to the model API.
     const llmResponse = await generate({
       prompt: `Suggest an item for the menu of a ${subject} themed restaurant`,
       model: 'ollama/gemma',
       config: {
         temperature: 1,
       },
-    });
+    })
 
-		// Handle the response from the model API. In this sample, we just convert
-    // it to a string, but more complicated flows might coerce the response into
-    // structured output or chain the response into another LLM call, etc.
-    return llmResponse.text();
+    return llmResponse.text()
   }
-);
+)
 
-// Start a flow server, which exposes your flows as HTTP endpoints. This call
-// must come last, after all of your plug-in configuration and flow definitions.
-// You can optionally specify a subset of flows to serve, and configure some
-// HTTP server options, but by default, the flow server serves all defined flows.
-startFlowsServer();
+startFlowsServer()
